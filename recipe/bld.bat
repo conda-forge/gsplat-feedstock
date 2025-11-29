46b508514b1af2f6688c187eb9aa62ef8f62f816
Jeongseok Lee <jeongseok@meta.com>
2025-11-29 13:09:10 -0800

    Fix syntax error in bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -18,15 +18,15 @@ set "CG_PATCH=%SRC_DIR%\cg_fix.h"
     echo #include ^<cooperative_groups.h^>
     echo namespace cooperative_groups {
     echo     template^<class Group^> __device__ __forceinline__
-    echo     auto labeled_partition^^(const Group^^^& g, unsigned int id^^) ^{
-    echo         return experimental::labeled_partition^^(g, id^^);
+    echo     auto labeled_partition^(const Group^^^& g, unsigned int id^) ^{
+    echo         return experimental::labeled_partition^(g, id^);
     echo     }
     echo }
 )
 
 REM  Make the compilers force-include it
-set "CXXFLAGS=%CXXFLAGS% /FI\"%CG_PATCH%\""
-set "NVCC_FLAGS=%NVCC_FLAGS% -include \"%CG_PATCH%\""
+set "CXXFLAGS=%CXXFLAGS% /FI"%CG_PATCH%""
+set "NVCC_FLAGS=%NVCC_FLAGS% -include "%CG_PATCH%""
 
 REM ──────────────────────────────────────────────────────────────────────
 REM 3.  Build + install
